7f87542695b5b809c091d06d948e4802bc6278bf
hadamo <hadamosieg@gmail.com>
2020-07-30 13:38:42 -0300

    Setup

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="FakeFlix Logo"/>
+            </a>
+            {/* <ButtonLink className="ButtonLink" href="/">
+                Novo Vídeo
+            </ButtonLink> */}
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
