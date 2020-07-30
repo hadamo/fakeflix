f15534bde229cdfdaae7cb1120b987985deab667
hadamo <hadamosieg@gmail.com>
2020-07-30 20:38:04 -0300

    Aula 2, roteamento, state e slider

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -7,13 +8,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="FakeFlix Logo"/>
-            </a>
-            {/* <ButtonLink className="ButtonLink" href="/">
-                Novo Vídeo
-            </ButtonLink> */}
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
